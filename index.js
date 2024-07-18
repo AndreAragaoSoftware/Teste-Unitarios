8dc882948e4cbed99d0d4c5e13243eb6fddaefd1
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-07-18 12:31:28 +0100

    Primeiro teste

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        // eslint-disable-next-line no-throw-literal
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', () => {
+  const esperando = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperando);
+});
+
+teste('calculaDesconto', () => {
+  const esperado = 2200;
+  const retornado = calculaDescontos(2500, 300);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
